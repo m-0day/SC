4eaca90def44ad989dc3576df47a0ef51583835e
Chris McLean <chris.mclean@cho.elderresearch.com>
2020-03-20 13:10:02 -0400

    first commit

---
+++ clean.py
@@ -0,0 +1,9 @@
+### Developed by Chris McLean ###
+# Mar 2020
+# For more hobby projects of varying degrees of completion see my github
+# https://www.github.com/m-0day
+
+import pandas as pd 
+import numpy as np 
+
+df = pd.read_csv('marketing_training.csv')
